2f91555cfd44949b7606e7ddcfe40a9231b94531
Sanjyot Zade <zade.sanjyot@gmail.com>
2021-05-22 22:13:00 +0530

    adding 5min break for internet start

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import socket
 from time import sleep
 from subprocess import call
@@ -33,6 +34,14 @@ def restart_network():
 
 
 if __name__ == "__main__":
+    
+    try:
+        if sys.argv[1]:
+           print("Waiting for internet start")
+           sleep(60*4)
+    except:
+        pass
+
     while not is_connected(REMOTE_SERVER):
         print("Waiting for internet...")
         sleep(40)
